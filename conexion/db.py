a5241a160b3f6768c78b7aae631696d73b46ad7b
Jose Eduardo Orozco Cardenas <72564650+Eduardo-Orozco-67@users.noreply.github.com>
2023-11-15 10:32:26 -0600

    Update db.py

--- conexion/db.py
+++ conexion/db.py
@@ -4,9 +4,9 @@ import psycopg2
 def get_connection():
     connection = psycopg2.connect(
         dbname="constructora",
-        user="postgres",
-        password="12345",
-        host="localhost",
+        user="constructora_user",
+        password="3mxhSYupgYNxm1L4Cs6V1k9UYLJBFyXS",
+        host="postgres://constructora_user:3mxhSYupgYNxm1L4Cs6V1k9UYLJBFyXS@dpg-claduqpm6hds73ehit2g-a/constructora",
         port="5432"
     )
     return connection
